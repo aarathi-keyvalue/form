433c3db64282176f09682cacd16c7114c98092ba
aarathi00 <aarathi.b@keyvalue.systems>
2023-08-03 17:42:52 +0530

    Add private and public layouts

--- src/routes/RouteLayout.jsx
+++ src/routes/RouteLayout.jsx
@@ -1,54 +1,18 @@
-import { useEffect, useState } from "react";
-import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import { routes } from "./routes";
-import { navConstants } from "../constants/common";
-import Form from "../pages/form/Form";
-import Country from "../pages/country/Country";
-import CountryDetail from "../pages/country-detail/CountryDetail";
+import PrivateLayout from "./PrivateLayout";
+import PublicLayout from "./PublicLayout";
 
 const RouteLayout = () => {
-  const [selectedTab, setSelectedTab] = useState();
-
-  const navigate = useNavigate();
-  const { pathname } = window.location;
-
-  useEffect(() => {
-    setSelectedTab("/" + pathname.split("/")[1]);
-  }, [pathname]);
-
-  const handleTabClick = (path) => {
-    setSelectedTab(path);
-    navigate(path);
-  };
+  const isAuthenticated = false;
 
   return (
     <div className="w-full h-[100vh] flex">
-      <div className="w-1/5 h-full px-2 py-20 flex flex-col gap-y-3 border-r">
-        {navConstants.map((navItem) => (
-          <div
-            key={navItem.path}
-            className={`w-full rounded-md px-5 py-2 hover:bg-harp/60 cursor-pointer ${
-              selectedTab === navItem.path
-                ? "text-primaryColor bg-hawksBlue/30"
-                : "text-black"
-            }`}
-            onClick={() => handleTabClick(navItem.path)}
-          >
-            {navItem.label}
-          </div>
-        ))}
-      </div>
       <Routes>
-        <Route path={routes.COUNTRY_LIST} element={<Country />} />
-        <Route path={routes.FORM} element={<Form />} />
-        <Route
-          path={`${routes.COUNTRY_LIST}/:country`}
-          element={<CountryDetail />}
-        />
         <Route
           path={routes.ALL}
-          element={<Navigate replace={true} to={routes.FORM} />}
+          element={isAuthenticated ? <PrivateLayout /> : <PublicLayout />}
         />
       </Routes>
     </div>
