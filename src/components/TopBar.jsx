78f62c547b08b066f6fa2fd34b98a67925637e3d
aarathi00 <aarathi.b@keyvalue.systems>
2023-08-16 12:03:45 +0530

    add drag and sort

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,14 +1,18 @@
 import { LeftArrow } from "../assets/icons";
+import Button from "./Button";
 
 const TopBar = (props) => {
   const {
     headerText,
     showNavigateBack = false,
     handleBackClick = () => {},
+    isButtonRequired = false,
+    buttonText = "",
+    onButtonClick = () => {},
   } = props;
   return (
-    <div className="w-full px-9 py-8 bg-white text-lg font-medium border-b">
-      <div className="flex items-center">
+    <div className="w-full flex justify-between px-9 py-8 bg-white text-lg font-medium border-b">
+      <div className="flex items-center w-1/2">
         {showNavigateBack && (
           <LeftArrow
             className="mr-3 cursor-pointer"
@@ -17,6 +21,15 @@ const TopBar = (props) => {
         )}
         {headerText}
       </div>
+      {isButtonRequired && (
+        <div className="w-fit flex justify-end">
+          <Button
+            label={buttonText}
+            buttonStyles="w-full bg-primaryColor text-white text-xs"
+            onClick={onButtonClick}
+          />
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/users/UsersListing.jsx
+++ src/pages/users/UsersListing.jsx
@@ -1,55 +1,25 @@
-import { useNavigate } from "react-router";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import { topBarConstants } from "../../constants/common";
-import { routes } from "../../routes/routes";
-import { GENDER_LABELS, QUALIFICATION_LABELS } from "../../constants/form";
-import { capitalise } from "../../utils/generalUtils";
-import {
-  CrossIcon,
-  DeleteLightIcon,
-  EditLightIcon,
-  TickIcon,
-} from "../../assets/icons";
-import { updateUser } from "../../store/form";
 import { NoDataFound } from "../../assets/images";
-import TopBar from "../../components/TopBar";
+import { SortableList, TopBar } from "../../components";
 
 const UsersListing = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
   const { usersList } = useSelector((state) => state.form);
 
-  const fontStyle = "font-light";
+  const handleSave = () => {
+    localStorage.setItem("listedUsers", JSON.stringify(usersList));
+  };
+
   const cellStyle = "w-1/6";
-
-  const editUser = (userId) => {
-    const user = usersList.find((user) => user.createdAt === userId);
-    navigate(`${routes.FORM}/${user.createdAt}`);
-  };
-
-  const deleteUser = (userId) => {
-    const users = usersList.filter((user) => user.createdAt !== userId);
-    localStorage.setItem("listedUsers", JSON.stringify(users));
-    dispatch(updateUser(users));
-  };
-
-  const disableUser = (userId) => {
-    const userIndex = usersList.findIndex((user) => user.createdAt === userId);
-    const user = usersList[userIndex];
-    const updatedUserList = [...usersList];
-    updatedUserList.splice(userIndex, 1, {
-      ...user,
-      isActive: !user.isActive,
-    });
-    localStorage.setItem("listedUsers", JSON.stringify(updatedUserList));
-    dispatch(updateUser(updatedUserList));
-  };
-
   return (
     <div className="w-full h-full">
-      <TopBar headerText={topBarConstants.USER_DETAILS} />
+      <TopBar
+        headerText={topBarConstants.USER_DETAILS}
+        isButtonRequired
+        buttonText="Save to Local"
+        onButtonClick={handleSave}
+      />
       <div className="w-full h-[calc(100vh-93px)] bg-harp p-4 overflow-y-auto sm:p-10">
         {usersList && usersList.length > 0 ? (
           <>
@@ -61,105 +31,8 @@ const UsersListing = () => {
               <div className={cellStyle}>Phone Number</div>
               <div className={cellStyle}>Actions</div>
             </div>
-            <div className="w-full flex flex-col gap-y-5">
-              {usersList.map((user) => (
-                <div
-                  key={user.createdAt}
-                  className={`flex items-center gap-x-5 px-5 py-4 rounded-md shadow-md ${
-                    user.isActive
-                      ? "bg-white hover:scale-[99%] cursor-pointer"
-                      : ""
-                  }`}
-                  onClick={() => {
-                    if (user.isActive) {
-                      navigate(`${routes.USERS}/${user.createdAt}`);
-                    }
-                  }}
-                >
-                  <div className={`flex gap-x-3 items-center ${cellStyle}`}>
-                    <div className="w-12 h-12 rounded-full bg-white">
-                      <img
-                        src={user.image}
-                        alt="profile"
-                        className="object-contain w-full h-full rounded-full"
-                      />
-                    </div>
-                    <div
-                      className={`${fontStyle} ${
-                        user.isActive ? "text-black" : "text-comet"
-                      }`}
-                    >
-                      {capitalise(user.name)}
-                    </div>
-                  </div>
-                  <div
-                    className={`${fontStyle} ${cellStyle} ${
-                      user.isActive ? "text-black" : "text-comet"
-                    }`}
-                  >
-                    {QUALIFICATION_LABELS[user.degree]}
-                  </div>
-                  <div
-                    className={`${fontStyle} ${cellStyle} ${
-                      user.isActive ? "text-black" : "text-comet"
-                    }`}
-                  >
-                    {GENDER_LABELS[user.gender]}
-                  </div>
-                  <div
-                    className={`${fontStyle} ${cellStyle} ${
-                      user.isActive ? "text-black" : "text-comet"
-                    }`}
-                  >
-                    {user.country}
-                  </div>
-                  <div
-                    className={`${fontStyle} ${cellStyle} ${
-                      user.isActive ? "text-black" : "text-comet"
-                    }`}
-                  >
-                    {user.phoneNumber}
-                  </div>
-                  <div className={`${cellStyle} flex items-center`}>
-                    <div
-                      className={`p-3 ${user.isActive ? "cursor-pointer" : ""}`}
-                      onClick={(e) => {
-                        if (user.isActive) {
-                          e.stopPropagation();
-                          editUser(user.createdAt);
-                        }
-                      }}
-                    >
-                      <EditLightIcon width={20} height={20} />
-                    </div>
-                    <div
-                      className={`p-3 ${user.isActive ? "cursor-pointer" : ""}`}
-                      onClick={(e) => {
-                        if (user.isActive) {
-                          e.stopPropagation();
-                          deleteUser(user.createdAt);
-                        }
-                      }}
-                    >
-                      <DeleteLightIcon width={18} height={18} />
-                    </div>
-                    <div
-                      className="p-3 cursor-pointer"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        disableUser(user.createdAt);
-                      }}
-                    >
-                      {user.isActive ? (
-                        <TickIcon height={18} width={18} />
-                      ) : (
-                        <CrossIcon height={16} width={16} />
-                      )}
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
+
+            <SortableList listItems={usersList} />
           </>
         ) : (
           <div className="w-full bg-white min-h-full rounded-sm shadow-sm items-center justify-center flex flex-col">
